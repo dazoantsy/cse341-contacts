b5c8fa281c69dc07dac45d2bf23be01d9c8112a6
Njato Harizo Ragodonandrasana <dazoantsy@gmail.com>
2026-01-14 23:44:29 +0300

    Step 2: Express Hello World

--- server.js
+++ server.js
@@ -1,13 +1,8 @@
-const express = require("express");
-require("dotenv").config();
-
+const express = require('express');
 const app = express();
-const port = process.env.PORT || 3000;
 
-app.get("/", (req, res) => {
-  res.send("CSE 341 Contacts API");
+app.get('/', (req, res) => {
+  res.send('Hello World');
 });
 
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
-});
+app.listen(3000);
